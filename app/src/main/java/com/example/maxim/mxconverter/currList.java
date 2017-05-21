5a4991715c0a1c64e414730146921796dfbe3dd4
1MaXimus <info@vidates.ru>
2017-05-22 01:10:38 +0600

    commit1

---
+++ app/src/main/java/com/example/maxim/mxconverter/currList.java
@@ -0,0 +1,15 @@
+package com.example.maxim.mxconverter;
+
+import java.util.ArrayList;
+
+
+public class currList {
+
+    public ArrayList<Curren> list;
+    public String date;
+
+    public currList(ArrayList<Curren> l, String d){
+        this.list = l;
+        this.date = d;
+    }
+}
